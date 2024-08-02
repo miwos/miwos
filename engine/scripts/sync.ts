71650705eac2dd7980a7695f002784da8973f559
arnoson <hallo@arno-schlipf.info>
2024-08-02 21:07:36 +0200

    fix: only setup watcher once

--- engine/scripts/sync.ts
+++ engine/scripts/sync.ts
@@ -2,54 +2,52 @@ import { watch } from 'chokidar'
 import { readFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
 import { WebSocket, WebSocketServer } from 'ws'
+import mitt, { Emitter } from 'mitt'
 // @ts-ignore (missing types)
 import launch from 'launch-editor'
+
+type Message = { id: number; method: string; file?: string }
+type Events = { message: Message }
 
 const pathToPosix = (path: string) => path.replace(/\\/g, '/')
 
 const wss = new WebSocketServer({ port: 8080 })
-let initialFilesSynced = false
 let requestId = 0
-let prevSocket: WebSocket | undefined
 
-wss.on('connection', (socket) => {
-  // For now we allow only one active connection.
-  prevSocket?.close()
-  prevSocket = socket
+const emitter: Emitter<Events> = mitt()
+const watcher = watch('**/*', { cwd: resolve(process.cwd(), 'src') })
+let filesToSync: string[] = []
+const handleInitialAdd = (path: string) => {
+  const isProject = pathToPosix(path).startsWith('projects/')
+  if (!isProject) filesToSync.push(path)
+}
+watcher.on('add', handleInitialAdd)
+watcher.on('ready', async () => watcher.off('add', handleInitialAdd))
 
-  const waitForResponse = (id: number) =>
-    new Promise<void>((resolve, reject) => {
-      const handler = (buffer: ArrayBuffer) => {
-        const data = JSON.parse(new TextDecoder().decode(buffer))
-        if (id === data.id) {
-          socket.off('message', handler)
-          resolve()
-        }
-      }
-      socket.on('message', handler)
-      setTimeout(() => reject('response timeout'), 3000)
-    })
-
+const setupConnection = (socket: WebSocket) => {
   socket.on('message', async (buffer: ArrayBuffer) => {
     const data = JSON.parse(new TextDecoder().decode(buffer))
+    emitter.emit('message', data)
+  })
 
-    if (data.method === 'deviceConnected' && !initialFilesSynced) {
+  emitter.on('message', async ({ method, file }: Message) => {
+    if (method === 'deviceConnected') {
       for (let path of filesToSync) {
         await syncFile(path, false)
-        console.log('sync', path)
       }
-      initialFilesSynced = true
+      filesToSync = []
       return
     }
 
-    if (data.method === 'launchEditor') {
-      const file = data.file.replace(/^lua\//, 'src/')
+    if (method === 'launchEditor') {
+      file = file!.replace(/^lua\//, 'src/')
       launch(file, 'code')
     }
   })
 
   const syncFile = async (path: string, update = true) => {
     path = pathToPosix(path)
+    console.log('sync', path)
     const content = await readFile(resolve('src', path), 'utf8')
     const method = update ? 'updateFile' : 'writeFile'
     const id = requestId++
@@ -61,14 +59,31 @@ wss.on('connection', (socket) => {
     }
   }
 
-  const watcher = watch('**/*', { cwd: resolve(process.cwd(), 'src') })
+  const waitForResponse = (id: number) =>
+    new Promise<void>((resolve, reject) => {
+      const handler = (data: Message) => {
+        if (id === data.id) {
+          emitter.off('message', handler)
+          resolve()
+        }
+      }
+      emitter.on('message', handler)
+      setTimeout(() => reject('response timeout'), 3000)
+    })
+
   watcher.on('change', syncFile)
 
-  const filesToSync: string[] = []
-  const handleInitialAdd = (path: string) => {
-    const isProject = pathToPosix(path).startsWith('projects/')
-    if (!isProject) filesToSync.push(path)
+  const cleanup = () => {
+    socket.close()
+    emitter.all.clear()
+    watcher.off('change', syncFile)
   }
-  watcher.on('add', handleInitialAdd)
-  watcher.on('ready', async () => watcher.off('add', handleInitialAdd))
+
+  return { cleanup }
+}
+
+let connection: ReturnType<typeof setupConnection> | undefined
+wss.on('connection', (socket) => {
+  connection?.cleanup()
+  connection = setupConnection(socket)
 })
